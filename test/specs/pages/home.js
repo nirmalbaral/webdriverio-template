1d9e9ce603d67deb9b5bb6747004bfa83cafae33
nirmal baral <nirmal.baral@virginmedia.co.uk>
2019-05-16 12:32:40 +0100

    update

--- test/specs/pages/home.js
+++ test/specs/pages/home.js
@@ -4,22 +4,22 @@ var homePage = require('../../pageobjects/home.page');
 
 describe('webdriver.io menu links', function () {
 
-    it('should show home page when home link is lol clicked', function () {
+    it('should show home page when home link is clicked', function () {
         homePage.open();
         homePage.homepageLink.click();
 
-        expect(homePage.pageTitle()).to.contain('WebdriverIO - WebDriver bindings for Node.js');
+        expect(homePage.pageTitle()).to.contain('API Docs · WebdriverIO');
     });
 
     it('should show documentation page when developer guide link is clicked', function () {
         homePage.guideLink.click();
 
-        expect(homePage.title()).to.contain('WebdriverIO - Developer Guide');
+        expect(homePage.title()).to.contain('WebdriverIO · Next-gen WebDriver test framework for Node.js');
     });
 
     it('should show api page when api link is clicked', function () {
         homePage.apiLink.click();
 
-        expect(homePage.title()).to.contain('WebdriverIO - API Docs');
+        expect(homePage.title()).to.contain('Blog · WebdriverIO');
     });
 });
